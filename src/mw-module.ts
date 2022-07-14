1a4161e1e821efb3443f77817e4dd7dbf20972c1
KunYao-Fu <wayne@zuso.ai>
2022-07-14 11:15:55 +0800

    fix module wrong import

--- src/mw-module.ts
+++ src/mw-module.ts
@@ -3,8 +3,8 @@ import {NgModule} from '@angular/core';
 import {FormModule} from './modules/form/form.module';
 import {BootstrapFormModule} from './modules/bootstrap-form/bootstrap-form.module';
 import {BootstrapTableModule} from './modules/bootstrap-table/bootstrap-table.module';
-import {SpinnerModule} from './modules/spinner/spinner.module';
 import {EventListenerModule} from './modules/event-listener/event-listener.module';
+import {LoadingModule} from './modules/loading/loading.module';
 
 
 
@@ -12,7 +12,7 @@ const MODULES = [
   FormModule,
   BootstrapFormModule,
   BootstrapTableModule,
-  SpinnerModule,
+  LoadingModule,
   EventListenerModule,
 ];
 
